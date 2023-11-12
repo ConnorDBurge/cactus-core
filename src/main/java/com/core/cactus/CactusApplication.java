d13dbbea0b960f4778f60f4e34920ed9eb7fb4b3
ConnorDBurge <connor.d.burge@gmail.com>
2023-11-12 12:01:06 -0500

    DB and Docker set up

---
+++ src/main/java/com/core/cactus/CactusApplication.java
@@ -0,0 +1,12 @@
+package com.core.cactus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CactusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CactusApplication.class, args);
+	}
+}
---
+++ src/test/java/com/core/cactus/CactusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.cactus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CactusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
